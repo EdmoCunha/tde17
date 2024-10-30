92a0b2d84660a104ef746ae54c0a97b67056f860
EDMO CUNHA PITHAN <165856288+EdmoCunha@users.noreply.github.com>
2024-10-29 21:30:01 -0300

    Add files via upload

---
+++ tde17/Program.cs
@@ -0,0 +1,8 @@
+﻿System.Console.WriteLine(ConversorTemperatura.CelsiusParaFahrenheit(39));
+System.Console.WriteLine(ConversorTemperatura.FahrenheitParaCelsius(102.5));
+
+Configuracoes.NomeAplicacao ="Laboratorio de programação";
+Configuracoes.Versao =5;
+
+System.Console.WriteLine(Configuracoes.NomeAplicacao);
+System.Console.WriteLine(Configuracoes.Versao);
